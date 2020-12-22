23b9293ffe41c40066cbe4f2d3e23e0dd8de9606
Ashish Seervi <seerviashish17@gmail.com>
2020-12-22 16:03:11 +0530

    init

---
+++ src/main/java/com/instahire/parkinglot/ParkingLotApplication.java
@@ -0,0 +1,10 @@
+package com.instahire.parkinglot;
+
+
+public class ParkingLotApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/test/java/com/instahire/parkinglot/ParkingLotApplicationTests.java
@@ -0,0 +1,6 @@
+package com.instahire.parkinglot;
+
+
+class ParkingLotApplicationTests {
+
+}
